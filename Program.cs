c3fed85e45ae6ca5a98dfe5854fec60fab936d1e
Cabral2104 <valdezemilio50@gmail.com>
2023-08-31 16:32:50 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursividadProblema
+{
+    internal class Program
+    {
+        static void GenerarPermutaciones(string cadena, string prefijo = "")
+        {
+            int n = cadena.Length;
+
+            if (n == 0)
+            {
+                Console.WriteLine(prefijo); // Imprimir permutación completa
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    string nuevaCadena = cadena.Substring(0, i) + cadena.Substring(i + 1);
+                    GenerarPermutaciones(nuevaCadena, prefijo + cadena[i]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string entrada = "abc";
+            Console.WriteLine("Permutaciones de " + entrada + ":");
+            GenerarPermutaciones(entrada);
+            Console.ReadKey();
+        }
+    }
+}
